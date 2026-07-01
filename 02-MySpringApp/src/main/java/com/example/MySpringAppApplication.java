0d87ae089e9898d82061679873497f50f0bac1d6
Alparslan Tarkan <atarkan@hotmail.com>
2026-07-01 16:59:57 +0200

    xml-based configuration

--- 02-MySpringApp/src/main/java/com/example/MySpringAppApplication.java
+++ 02-MySpringApp/src/main/java/com/example/MySpringAppApplication.java
@@ -1,15 +1,15 @@
 package com.example;
 
-import com.example.config.AppConfig;
 import com.example.core.PaymentService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MySpringAppApplication {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        PaymentService paymentService = context.getBean(PaymentService.class);
+        PaymentService paymentService = context.getBean("paymentService", PaymentService.class);
+
         paymentService.processPayment();
 
         context.close();
--- 02-MySpringApp/src/main/java/com/example/core/PaymentService.java
+++ 02-MySpringApp/src/main/java/com/example/core/PaymentService.java
@@ -1,28 +1,7 @@
 package com.example.core;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-import org.springframework.stereotype.Component;
-
-@Component
-//@Scope("prototype")
 public class PaymentService {
-
-    public PaymentService() {
-        System.out.println("1. Constructor of the PaymentService is called");
-    }
-
-    @PostConstruct
-    public void init() {
-        System.out.println("2. The bean initialized");
-    }
-
     public void processPayment() {
         System.out.println("3. Bean is being used.");
-    }
-
-    @PreDestroy
-    public void destroy() {
-        System.out.println("4. Bean is being destroyed.");
     }
 }
