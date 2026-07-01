fcb837dbbc0694e87171869a3e6be2b142ee6060
Alparslan Tarkan <atarkan@hotmail.com>
2026-07-01 18:04:19 +0200

    component scanning in practice

---
+++ 02-ComponentScanning/src/main/java/com/example/MySpringAppApplication.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import com.example.config.AppConfig;
+import com.example.core.OrderService;
+import com.example.core.PaymentService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MySpringAppApplication {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        PaymentService paymentService = context.getBean(PaymentService.class);
+
+        paymentService.processPayment();
+
+        OrderService orderService = context.getBean(OrderService.class);
+
+        orderService.placeOrder();
+
+        context.close();
+    }
+
+}
