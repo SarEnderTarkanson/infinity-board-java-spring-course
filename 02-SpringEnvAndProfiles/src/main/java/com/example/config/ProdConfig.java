f4ba65bcf09387407f247908cb08ad82e4fca9a2
Alparslan Tarkan <atarkan@hotmail.com>
2026-07-03 18:20:41 +0200

    managing environment properties

---
+++ 02-SpringEnvAndProfiles/src/main/java/com/example/config/ProdConfig.java
@@ -0,0 +1,14 @@
+package com.example.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan(basePackages = "com.example")
+@PropertySource("classpath:application-prod.properties")
+@Profile("prod")
+public class ProdConfig {
+
+}
