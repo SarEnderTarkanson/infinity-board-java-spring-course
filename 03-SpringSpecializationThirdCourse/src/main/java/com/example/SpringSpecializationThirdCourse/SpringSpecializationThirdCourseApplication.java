3f8e36d83ae3c8fa8488188408b816479b0da2f3
Alparslan Tarkan <atarkan@hotmail.com>
2026-07-21 17:47:13 +0200

    autoconfiguration explained

---
+++ 03-SpringSpecializationThirdCourse/src/main/java/com/example/SpringSpecializationThirdCourse/SpringSpecializationThirdCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSpecializationThirdCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpecializationThirdCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSpecializationThirdCourseApplication.class, args);
+	}
+
+}
---
+++ 03-SpringSpecializationThirdCourse/src/test/java/com/example/SpringSpecializationThirdCourse/SpringSpecializationThirdCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSpecializationThirdCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpecializationThirdCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
