3d30fd2f691abaf968f077ae5b18b4b87c61ac28
Alparslan Tarkan <atarkan@hotmail.com>
2026-07-23 09:00:28 +0200

    anatomy of a spring boot project

---
+++ 03-SpringSpecializationThirdCourse/src/main/java/com/example/SpringSpecializationThirdCourse/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.SpringSpecializationThirdCourse.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    @GetMapping("/welcome")
+    public String Welcome() {
+        return "Welcome to the user API";
+    }
+
+}
