3f41dcc343c8feed796c0aaf9366c83c79acbaa9
Alparslan Tarkan <atarkan@hotmail.com>
2026-07-27 08:11:46 +0200

    application.yml in depth

---
+++ 03-SpringSpecializationThirdCourse/src/main/java/com/example/SpringSpecializationThirdCourse/config/AppConfig.java
@@ -0,0 +1,4 @@
+package com.example.SpringSpecializationThirdCourse.config;
+
+public class AppConfig {
+}
---
+++ 03-SpringSpecializationThirdCourse/src/main/java/com/example/SpringSpecializationThirdCourse/controller/ApplicationController.java
@@ -0,0 +1,21 @@
+package com.example.SpringSpecializationThirdCourse.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+
+    @Value("${app.name:DefaultAppName}")
+    private String appName;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @GetMapping("/app")
+    public String app() {
+        return "Welcome to " + appName + " and the version is " + appVersion;
+    }
+
+}
