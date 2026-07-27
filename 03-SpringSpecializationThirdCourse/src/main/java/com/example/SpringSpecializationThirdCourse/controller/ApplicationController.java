eb55ccd556ba635138a739b0049a35a36b00405e
Alparslan Tarkan <atarkan@hotmail.com>
2026-07-27 08:25:57 +0200

    binding config to java classes

--- 03-SpringSpecializationThirdCourse/src/main/java/com/example/SpringSpecializationThirdCourse/controller/ApplicationController.java
+++ 03-SpringSpecializationThirdCourse/src/main/java/com/example/SpringSpecializationThirdCourse/controller/ApplicationController.java
@@ -1,11 +1,18 @@
 package com.example.SpringSpecializationThirdCourse.controller;
 
+import com.example.SpringSpecializationThirdCourse.config.AppConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApplicationController {
+
+    private final AppConfig appConfig;
+
+    public ApplicationController(AppConfig appConfig) {
+        this.appConfig = appConfig;
+    }
 
     @Value("${app.name:DefaultAppName}")
     private String appName;
@@ -16,6 +23,11 @@ public class ApplicationController {
     @GetMapping("/app")
     public String app() {
         return "Welcome to " + appName + " and the version is " + appVersion;
+    }
+
+    @GetMapping("/config")
+    public String config() {
+        return "Welcome to App Config" +  appConfig.getName() + ", Version: " + appConfig.getVersion() + ", Author: " + appConfig.getAuthor();
     }
 
 }
