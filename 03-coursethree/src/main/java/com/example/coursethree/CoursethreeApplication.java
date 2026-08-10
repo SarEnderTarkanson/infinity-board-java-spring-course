89f18d01cd0294934d02958209ef3083605f9d94
Alparslan Tarkan <atarkan@hotmail.com>
2026-08-10 07:30:30 +0200

    creating service contracts

---
+++ 03-coursethree/src/main/java/com/example/coursethree/CoursethreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursethree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursethreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursethreeApplication.class, args);
+	}
+
+}
