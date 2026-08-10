7a1c013307ca2f0db1d7bad1d8ace05a84b41776
Alparslan Tarkan <atarkan@hotmail.com>
2026-08-10 17:53:53 +0200

    handling pagination and sorting

---
+++ 03-coursethree/src/main/java/com/example/coursethree/config/AppConfig.java
@@ -0,0 +1,46 @@
+package com.example.coursethree.config;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component
+@ConfigurationProperties(prefix = "app")
+@Validated
+public class AppConfig {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String version;
+
+    @NotBlank
+    private String author;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
--- 03-coursethree/src/main/java/com/example/coursethree/controller/ApplicationController.java
+++ 03-coursethree/src/main/java/com/example/coursethree/controller/ApplicationController.java
@@ -1,11 +1,18 @@
 package com.example.coursethree.controller;
 
+import com.example.coursethree.config.AppConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApplicationController {
+
+    private final AppConfig appConfig;
+
+    public ApplicationController(AppConfig appConfig) {
+        this.appConfig = appConfig;
+    }
 
     @Value("${app.name:DefaultAppName}")
     private String appName;
@@ -16,6 +23,11 @@ public class ApplicationController {
     @GetMapping("/app")
     public String app() {
         return "Welcome to the " + appName + " ma" +  "and the app version is: " + appVersion;
+    }
+
+    @GetMapping("/config")
+    public String config() {
+        return "Application name: " + appConfig.getName() + " Application version: " + appConfig.getVersion() + " Application's author: "  + appConfig.getAuthor();
     }
 
 }
